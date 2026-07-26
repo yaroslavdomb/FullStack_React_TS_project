3ae266443d816983f9b741082ec64a60954c15a2
Yaroslav Domb <yaroslav.domb@gmail.com>
2026-07-26 23:49:10 +0300

    DEV - add service and model to connect to DB

---
+++ src/models/Book.ts
@@ -0,0 +1,8 @@
+export interface Book {
+  _id: string;
+  title: string;
+  author: string;
+  description: string;
+  coverUrl: string;
+  isFavorite: boolean;
+}
---
+++ src/services/api-service.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import type { Book } from '../models/Book';
+
+const _axios = axios.create({
+  baseURL: 'https://6a65fa9f06b3848d4b86f4bc.mockapi.io/api/v1/',
+  timeout: 10000,
+  headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+});
+
+export const ApiService = {
+  getAllBooks: () => _axios.get<Array<Book>>(`books`),
+  getBookById: (id: string) => _axios.get<Book>(`books/${id}`),
+  addNewBook: (newBook: Book) => _axios.post<Book>(`books`, newBook),
+  deleteBook: (id: string) => _axios.delete<Book>(`books/${id}`),
+  editBook: (editedBook: Book) => _axios.put<Book>(`books/${editedBook._id}`, editedBook),
+};
